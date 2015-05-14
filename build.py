91fbcadc2e9a8ceae4367ad42696d7053968ef19
liuxiong332 <liuxiong332@163.com>
2015-05-14 17:18:04 +0800

    add motor(mongodb driver) dependency

--- build.py
+++ build.py
@@ -7,10 +7,8 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
-
 name = "python-vote-server"
 default_task = "publish"
-
 
 @init
 def set_properties(project):
